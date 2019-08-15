45c60bc4cd92466941d3b03e819b6b626f48a974
eamiear <justcall_me_k@163.com>
2019-08-15 14:49:32 +0800

    fix设备类型检测

--- src/modules/typeHints.js
+++ src/modules/typeHints.js
@@ -2,7 +2,7 @@
  * @Author: eamiear
  * @Date: 2019-08-12 11:22:42
  * @Last Modified by: eamiear
- * @Last Modified time: 2019-08-12 11:33:23
+ * @Last Modified time: 2019-08-15 14:49:08
  */
 import Suiter from './suiter'
 import Converter from './converter'
@@ -33,10 +33,17 @@ class TypeHints {
    * this.isSensor = (type, subType) => {}
    */
   __rootProcessor () {
-    const typeList = Array.from(Object.keys(Suiter)).map(item => item.slice(0, 1).toUpperCase() + item.slice(1))
-    typeList.forEach(item => {
-      this[`is${item}`] = (deviceType, deviceSubType) => {
-        return this.__handler(Suiter[item.toLocaleLowerCase()], deviceType, deviceSubType)
+    // const typeList = Array.from(Object.keys(Suiter)).map(item => item.slice(0, 1).toUpperCase() + item.slice(1))
+    // typeList.forEach(item => {
+    //   this[`is${item}`] = (deviceType, deviceSubType) => {
+    //     return this.__handler(Suiter[item.toLocaleLowerCase()], deviceType, deviceSubType)
+    //   }
+    // })
+    Array.from(Object.keys(Suiter)).map(item => {
+      const types = Suiter[item].type
+      const keyCapital = item.slice(0, 1).toUpperCase() + item.slice(1)
+      this[`is${keyCapital}`] = (deviceType, deviceSubType) => {
+        return this.__handler(types, deviceType, deviceSubType)
       }
     })
   }
