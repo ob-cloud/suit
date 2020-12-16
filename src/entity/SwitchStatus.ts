48e8fbbeb5b3504cab0c9a1679f872e711fc722c
eamiear <justcall_me_k@163.com>
2020-12-16 17:52:15 +0800

    开关

--- src/entity/SwitchStatus.ts
+++ src/entity/SwitchStatus.ts
@@ -2,18 +2,42 @@
  * @Author: eamiear
  * @Date: 2020-08-29 17:46:03
  * @Last Modified by: eamiear
- * @Last Modified time: 2020-08-29 20:10:41
+ * @Last Modified time: 2020-12-16 17:32:47
  */
 
+import { Converter } from '../utils/converter';
 import { Status } from './Status';
 /**
  * TODO 开关
  */
 export class SwitchStatus extends Status {
   public state: string = '';
-  constructor (status: string) {
+  count: number = 1;
+  keyDots: string[];
+  constructor (status: string, count?: number) {
     super(status)
-    this.state = status.slice(0, 2)
+    this.state = status.slice(0, 2) || '00'
+    this.count = count || 1
+    this.keyDots = this.getKeyDots()
+  }
+  private getKeyDots () {
+    const keyList = []
+    const bits = new Converter(this.state, 16).toBinary()
+    let i = 0
+    for (let index = 0; index < this.count; index++) {
+      keyList[index] = bits.slice(i, i + 2)
+      i += 2
+    }
+    this.keyDots = keyList
+    return keyList
+  }
+  setKeyDot (v: string, index: number) {
+    if (index + 1 > this.count) return
+    this.keyDots[index] = v.toEven()
+  }
+  getKeyDotByIndex (index: number) {
+    if (index + 1 > this.count) return
+    return this.keyDots[index]
   }
   getState () {
     return this.state
--- src/modules/SwitchEquip.ts
+++ src/modules/SwitchEquip.ts
@@ -2,15 +2,51 @@
  * @Author: eamiear
  * @Date: 2020-08-29 20:16:40
  * @Last Modified by: eamiear
- * @Last Modified time: 2020-08-31 09:57:44
+ * @Last Modified time: 2020-12-16 17:36:44
  */
+import { SwitchStatus } from '../entity/SwitchStatus';
 import { BaseEquip } from './BaseEquip';
 
+enum SwitchKeyStatus {
+  ON = 1,
+  OFF = 0
+}
+
+const SwitchKeyStatusMap: any = {
+  [SwitchKeyStatus.ON]: '开',
+  [SwitchKeyStatus.OFF]: '关'
+}
+
 export class SwitchEquip extends BaseEquip {
+  switchStatus: SwitchStatus;
+  keyList: any;
+  private readonly bytes = `{0}000000000000000`;
   constructor(status: string, deviceType?: string, deviceChildType?: string) {
     super(status, deviceType, deviceChildType)
+    this.keyList = this.TypeHints.getSocketSwitchTypeIndex(deviceType, deviceChildType)
+    this.switchStatus = new SwitchStatus(status, +this.getKeyCount())
   }
-  getBytes (status: Array<string>) {
-    console.log(status)
+  get keyDots () {
+    // ['00', '01', '10', '11']
+    return this.switchStatus.keyDots
+  }
+  get keyCount () {
+    return +this.getKeyCount()
+  }
+  getKeyCount () {
+    if (!this.keyList || !this.keyList.length) return 1
+    let keys = this.keyList.split('|')
+    keys = keys.filter((it: any) => it)
+    return keys.reduce((a:any, b:any) => +a + (+b)) || 1
+  }
+  setPower (v: number, index: number) {
+    const vb = new this.Converter(`${v}`, 10).toBinary()
+    this.switchStatus.setKeyDot(vb, index)
+  }
+
+  getBytes () {
+    const keyDots = this.switchStatus.keyDots
+    const status = new this.Converter(keyDots.join(''), 2).toHex()
+    return this.bytes.format(status)
   }
 }
